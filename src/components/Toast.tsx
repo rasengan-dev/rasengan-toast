e17a7468ce4f3bacb4a0b89e5002c87ee2a7984c
dilane3 <komboudilane125@gmail.com>
2024-01-05 02:22:15 +0100

    Defining the two base component, Toast and ToastContainer

---
+++ src/components/Toast.tsx
@@ -0,0 +1,10 @@
+import { ToastProps } from "../types/index.js"
+
+/**
+ * Toast component
+ * @param param0 Props of the toast
+ * @returns 
+ */
+export default function Toast({ data }: ToastProps) {
+  return <div>{/** Something here **/}</div>;
+}
---
+++ src/components/ToastContainer.tsx
@@ -0,0 +1,9 @@
+import { ToastContainerProps } from "../types";
+
+/**
+ * ToastContainer component
+ * @returns JSX.Element
+ */
+export default function ToastContainer(props: ToastContainerProps) {
+  return <section {...props}>{/* Add something here */}</section>;
+}
